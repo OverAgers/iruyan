6463e781fbac8fa5eaab6d716fec1281b539d7bb
9-sho-5 <hosho.kusu@icloud.com>
2024-10-25 12:07:11 +0900

    fix: lib 依存関係解消

--- iruyan-api/main.go
+++ iruyan-api/main.go
@@ -1,19 +1,27 @@
 package main
 
 import (
+	"iruyan-api/routes"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	//Ginフレームワークのデフォルトの設定を使用してルータを作成
+	// Ginのルータを作成
 	router := gin.Default()
 
-	// ルートハンドラの定義
+	// ルートにアクセスしたときに "Hello! IRUYAN" を表示
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "Hello, World!",
+			"message": "Hello! IRUYAN",
 		})
 	})
+
+	// ユーザー関連のルーティングを登録
+	routes.RegisterUserRoutes(router)
+
+	// ルーム関連のルーティングは今後追加
+	// routes.RegisterRoomRoutes(router)
 
 	// サーバー起動
 	router.Run(":8080")
