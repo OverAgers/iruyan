6bc1db5b5ac31db1d7805cdb86a484fe025efb98
9-sho-5 <hosho.kusu@icloud.com>
2024-10-25 12:09:37 +0900

    add: user routing

---
+++ iruyan-api/routes/user_routes.go
@@ -0,0 +1,33 @@
+package routes
+
+import (
+	"iruyan-api/handlers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func RegisterUserRoutes(router *gin.Engine) {
+	// ログイン画面表示
+	router.GET("/login", handlers.LoginPageHandler)
+
+	// ログイン処理
+	router.POST("/login", handlers.LoginHandler)
+
+	// 新規登録画面表示
+	router.GET("/register", handlers.RegisterPageHandler)
+
+	// 新規登録処理
+	router.POST("/register", handlers.RegisterHandler)
+
+	// ユーザー画面
+	router.GET("/u/:user_id", handlers.UserPageHandler)
+
+	// 1週間の作業日取得
+	router.GET("/u/:user_id/work_info", handlers.WorkInfoHandler)
+
+	// 一緒に居た時間
+	router.GET("/u/:user_id/together", handlers.TogetherTimeHandler)
+
+	// 集中ランキング
+	router.GET("/u/ranking", handlers.RankingHandler)
+}
