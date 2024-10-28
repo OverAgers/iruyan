8906fd6314be02371da5f00048cb811cc656e7e0
RyunosukeKurokawa <kuroryu.basket@gmail.com>
2024-10-29 01:25:10 +0900

    fix:ボタンとテキストフィールドの修正

--- iruyan-front/src/components/ui/button/authorization-button.tsx
+++ iruyan-front/src/components/ui/button/authorization-button.tsx
@@ -2,14 +2,14 @@ import { Button } from "@mui/material";
 
 type Props = {
   title: string;
-  link: string;
+  type : "submit" | "button" | "reset" | undefined;
 };
 
 export default function AuthorizationButton(Props: Props) {
   return (
     <Button
       variant="contained"
-      href={Props.link}
+      type= {Props.type}
       sx={{backgroundColor: "#7A8764", fontWeight: "bold", p:2, height: "5rem", width: "31rem"}}
     >
       {Props.title}
