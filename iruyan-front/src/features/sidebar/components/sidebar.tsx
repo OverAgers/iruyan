769606c9eef6bab920625bfbe5ff816db2e86859
RyunosukeKurokawa <kuroryu.basket@gmail.com>
2024-10-31 01:34:19 +0900

    Revert "update:現在時刻取得機能の修正"
    This reverts commit 33678b0b0c59fb40d71854b09e4c9cd4321d3130.

--- iruyan-front/src/features/sidebar/components/sidebar.tsx
+++ iruyan-front/src/features/sidebar/components/sidebar.tsx
@@ -2,10 +2,17 @@ import SidebarCardDown from "@/components/ui/card/sidebar-card-down";
 import SidebarCardUp from "@/components/ui/card/sidebar-card-up";
 import { Typography } from "@mui/joy";
 import { Box } from "@mui/material";
-import useCurrentTime from "@/hooks/time-hooks";
+import { useEffect, useState } from "react";
 
 export default function Sidebar() {
-  const currentTime = useCurrentTime();
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+    return () => clearInterval(timer);
+  }, []);
 
   return (
     <Box width={"385px"} justifySelf={"flex-end"}>
@@ -16,12 +23,10 @@ export default function Sidebar() {
           現在時刻
         </Typography>
         <Typography fontSize={"128px"} lineHeight={1}>
-          {currentTime
-            ? currentTime.toLocaleTimeString([], {
-                hour: "2-digit",
-                minute: "2-digit",
-              })
-            : "--:--"}
+          {currentTime.toLocaleTimeString([], {
+            hour: "2-digit",
+            minute: "2-digit",
+          })}
         </Typography>
       </Box>
     </Box>
