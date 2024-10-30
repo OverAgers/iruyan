f84269163af3dd57a26e715d50e5b138cd9d7912
RyunosukeKurokawa <kuroryu.basket@gmail.com>
2024-10-31 01:10:36 +0900

    update:新規登録機能の修正

--- iruyan-front/src/features/register/api/post-register.ts
+++ iruyan-front/src/features/register/api/post-register.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useCallback, useState } from "react";
-import useSWRMutation from "swr";
+import useSWRMutation from "swr/mutation";
 
 import z from "zod";
 
@@ -8,24 +8,37 @@ export const postRegisterSchema = z.object({
   userID: z.string().min(1, "required"),
   userName: z.string().min(1, "required"),
   password: z.string().min(1, "required"),
+  email: z.string().min(1, "required"),
 });
 
-export type PostLoginRequest = z.infer<typeof postRegisterSchema>;
+export type PostRegisterRequest = z.infer<typeof postRegisterSchema>;
 
 export default function UseRegister() {
-  const [request, setRequest] = useState<PostLoginRequest>();
-  const requestURL = process.env.NEXT_PUBLIC_API_BASE_URL +"/register";
-  const fetcher = useCallback(() => {
-    return axios
-      .post(requestURL, request)
-      .then(async (res) => {
-        return res.data;
-      })
-      .catch((error) => {
-        throw new Error(`CardCheckout Error: ${error.message}`);
-      });
-  }, [request, requestURL]);
+  // const requestURL = `${process.env.NEXT_PUBLIC_API_URL}/login`;
+  const requestURL = `http://localhost:8080/register`;
 
-  const { data, error, isLoading } = useSWRMutation(requestURL, fetcher);
-  return { data, error, isLoading, setRequest };
+  const fetcher = useCallback(
+    async (url: string, { arg }: { arg: PostRegisterRequest }) => {
+      try {
+        const res = await axios.post(url, arg);
+        return res.data;
+      } catch (error: any) {
+        throw new Error(`ログインエラー: ${error.message}`);
+      }
+    },
+    []
+  );
+
+  const {
+    data,
+    error,
+    isMutating: isLoading,
+    trigger,
+  } = useSWRMutation(requestURL, fetcher);
+
+  const register = (registerData: PostRegisterRequest) => {
+    trigger(registerData);
+  };
+
+  return { data, error, isLoading, register };
 }
