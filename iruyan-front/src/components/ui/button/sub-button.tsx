34d953abb0238803eeb40c23fcca51cc514d2ad8
RyunosukeKurokawa <kuroryu.basket@gmail.com>
2024-10-30 16:30:38 +0900

    update:サブボタンの実装

---
+++ iruyan-front/src/components/ui/button/sub-button.tsx
@@ -0,0 +1,34 @@
+import Button from "@mui/joy/Button";
+
+type Props = {
+  title: string;
+  link: string;
+  size: "xs" | "sm" | "md" | "lg" | "xl";
+};
+
+export default function SubButton(Props: Props) {
+  return (
+    <Button
+      component="a"
+      href={Props.link}
+      size="xl"
+      sx={{
+        color: "#7A8764",
+        backgroundColor: "#F7F4ED",
+        border: "5px solid #7A8764",
+        borderRadius: "8px",
+        "&:hover": {
+          backgroundColor: "#7A8764",
+          color: "#F7F4ED",
+        },
+        "&:active": {
+          backgroundColor: "#353A2B",
+          color: "#F7F4ED",
+          border: "5px solid #353A2B",
+        },
+      }}
+    >
+      {Props.title}
+    </Button>
+  );
+}
