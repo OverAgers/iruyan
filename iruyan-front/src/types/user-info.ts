2faa464c22d096ee0b48e5356740fa388b91625a
RyunosukeKurokawa <kuroryu.basket@gmail.com>
2024-11-10 18:28:29 +0900

    update:UserInfoの修正

--- iruyan-front/src/types/user-info.ts
+++ iruyan-front/src/types/user-info.ts
@@ -2,15 +2,15 @@ export type StatusType = 'working' | 'resting' | 'idle';
 
 export type UserInfo = {
   iruyanId: string;
-  name: string;
+  userName: string;
   email: string;
   avatarUrl?: string;
   task?: string;
   note?: string;
-  status: StatusType;
-  workTime: number;
-  restTime: number;
-  startTime: number;
+  status?: StatusType;
+  workTime?: number;
+  restTime?: number;
+  startTime?: number;
   cumulativeTime?: number;
   consecutiveDays?: number;
 };
