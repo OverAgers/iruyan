ff8f141b0ef1e020705efefc8e474de3315916c7
9-sho-5 <hosho.kusu@icloud.com>
2025-07-02 12:33:07 +0900

    delete: 命名の変更による修正

--- iruyan-api/repository/errors.go
+++
@@ -1,8 +0,0 @@
-package repository
-
-import "errors"
-
-var (
-	ErrDuplicateIruyanID = errors.New("iruyan_id is already taken")
-	ErrDuplicateEmail    = errors.New("email is already registered")
-)
